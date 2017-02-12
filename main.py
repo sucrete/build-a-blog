82aecbc701498702e37206845cbc6336df09c87b
William Andree <williamandree@gmail.com>
2017-02-12 00:53:40 -0600

    first step in building a blog

--- main.py
+++ main.py
@@ -14,12 +14,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+    def front_render(self, title="", body="", error=""):
+        self.render("front.html", title=title, body=body, error=error)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.front_render()
+
+    def post(self):
+        title = self.request.get("title")
+        body = self.request.get("body")
+
+        if title and body:
+            self.write("thanks, guy!")
+
+        else:
+            error = "the bros need both a title and a body before we post it, dudely"
+            self.front_render(title, body, error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage)
 ], debug=True)
