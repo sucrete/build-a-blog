399d276f1aad291081a4f61fabd6f83f5ff56990
William Andree <williamandree@gmail.com>
2017-02-16 11:11:46 -0600

    created a few more classes

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import os
 import webapp2
 import jinja2
@@ -22,6 +7,9 @@ from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -33,24 +21,20 @@ class Handler(webapp2.RequestHandler):
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
+
 class BlogPost(db.Model):
     title = db.StringProperty(required = True)
     post = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
 
+    # def render(self):
+    #     self._render_text = self.content.replace('\n', '<br>')
+    #     renturn render_str('blogformpage.html', p=self)
+
 class MainPage(Handler):
-    def front_render(self, title="", body="", error=""):
-        self.render("front.html", title=title, body=body, error=error)
-        all_posts = db.GqlQuery("SELECT * FROM BlogPost ORDER BY created desc")
-
     def get(self):
-        self.front_render()
-
-    def post(self):
-
-
-
-
+        posts = db.GqlQuery("SELECT * FROM BlogPost ORDER BY created desc limit 5")
+        self.render("front.html", posts=posts)
 
 class FormPage(Handler):
     def form_render(self, title="", body="", error=""):
@@ -66,13 +50,24 @@ class FormPage(Handler):
         if title and body:
             a = BlogPost(title = title, body = body)
             a.put() #this will store the post in the database
-            self.redirect("/")
+            self.redirect('/blog/%s' % str(p.key().id()))
 
         else:
             error = "the Bros need both a title and a body before we post it, dudely"
             self.form_render(title, body, error)
+class Permalink(Handler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("permalink.html", post = post)
 
 app = webapp2.WSGIApplication([
-    ('/blog', MainPage)
-    ('/newpost', FormPage)
+    ('/blog', MainPage),
+    ('/blog/newpost', FormPage),
+    webapp2.Route('/blog/<id:\d+>', Permalink),
 ], debug=True)
