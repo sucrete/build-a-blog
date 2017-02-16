fdf38a6b0fc3a11f2cda415b0e37adb04c3f00a9
William Andree <williamandree@gmail.com>
2017-02-16 15:36:29 -0600

    got formpage to redirect. want to save before losing

--- main.py
+++ main.py
@@ -8,19 +8,23 @@ from google.appengine.ext import db
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
-def blog_key(name = 'default'):
-    return db.Key.from_path('blogs', name)
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
-        self.response.write(*a, **kw)
+        self.response.out.write(*a, **kw)
 
     def render_str(self, template, **params):
-        t = jinja_env.get_template(template)
-        return t.render(params)
+        return render_str(template, **params)
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
+
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
 
 class BlogPost(db.Model):
     title = db.StringProperty(required = True)
@@ -29,7 +33,9 @@ class BlogPost(db.Model):
 
     # def render(self):
     #     self._render_text = self.content.replace('\n', '<br>')
-    #     renturn render_str('blogformpage.html', p=self)
+    #     return render_str('blogformpage.html', p=self)
+
+
 
 class MainPage(Handler):
     def get(self):
@@ -37,27 +43,25 @@ class MainPage(Handler):
         self.render("front.html", posts=posts)
 
 class FormPage(Handler):
-    def form_render(self, title="", body="", error=""):
-        self.render("blogformpage.html", title=title, body=body, error=error)
-
     def get(self):
-        self.form_render()
+        self.render("blogformpage.html")
 
     def post(self):
         title = self.request.get("title")
-        body = self.request.get("body")
+        post = self.request.get("post")
 
-        if title and body:
-            a = BlogPost(title = title, body = body)
+        if title and post:
+            a = BlogPost(title = title, post = post)
             a.put() #this will store the post in the database
-            self.redirect('/blog/%s' % str(p.key().id()))
+            self.redirect('/blog')
 
         else:
-            error = "the Bros need both a title and a body before we post it, dudely"
-            self.form_render(title, body, error)
+            error = "the Bros need a title AND a body before we can post it, dudely"
+            self.render("blogformpage.html", title=title, body=body, error=error)
+
 class Permalink(Handler):
     def get(self, post_id):
-        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        key = db.Key.from_path('BlogPost', int(post_id), parent=blog_key())
         post = db.get(key)
 
         if not post:
